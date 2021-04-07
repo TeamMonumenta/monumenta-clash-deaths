5af8feb1f5a3e18dfb6686392f69b0269b4438cc
Byron Marohn <Combustible@live.com>
2021-04-07 01:52:30 +0000

    Initial boilerplate
    Signed-off-by: Byron Marohn <Combustible@live.com>

---
+++ src/main/java/com/playmonumenta/clashdeaths/ClashDeaths.java
@@ -0,0 +1,10 @@
+package com.playmonumenta.clashdeaths;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClashDeaths extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("Enabled!");
+	}
+}
